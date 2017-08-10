d8a9e075a527237ed1060b4bf180122c7456a410
sunguangxin <sunguangxin@bilibili.com>
2017-08-10 17:04:02 +0800

    1.初步搭建工程壳. 2.添加GXPhone和GXHome两个framework

---
+++ GXUniversal/GXUniversal/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GXUniversal
+//
+//  Created by sunguangxin on 2017/8/10.
+//  Copyright © 2017年 sunguangxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GXPhone/GXPhoneAppDelegate.h>
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GXPhoneAppDelegate class]));
+    }
+}
---
+++ GXUniversal/GXUniversalTests/GXUniversalTests.m
@@ -0,0 +1,39 @@
+//
+//  GXUniversalTests.m
+//  GXUniversalTests
+//
+//  Created by sunguangxin on 2017/8/10.
+//  Copyright © 2017年 sunguangxin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GXUniversalTests : XCTestCase
+
+@end
+
+@implementation GXUniversalTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
